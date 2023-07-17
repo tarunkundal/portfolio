49d15ab105572106a7dd067e9407d93069c594b2
Tarun Chauhan <chauhan271singh@gmail.com>
2023-07-17 21:51:49 +0530

    Aboutmodified

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Flex,
   Heading,
   Image,
@@ -29,10 +30,19 @@ export const pulseRing = keyframes`
 const About = () => {
   return (
     <Stack
-      bgImage={
-        "url('https://img.freepik.com/free-vector/dark-background-with-green-wavy-shapes_1035-9068.jpg?size=626&ext=jpg&ga=GA1.1.823988085.1685946705&semt=ais')"
-      }
+      bgImage={useColorModeValue(
+        "url('https://img.freepik.com/free-vector/watercolor-emerald-background_23-2150270303.jpg?w=1800&t=st=1689221539~exp=1689222139~hmac=4616fc0609df6f63192c4f5b47f61b122aa40c52bea8274a3f1f43aadb7793f2')",
+        ""
+      )}
     >
+      <Box position={"absolute"} zIndex={-1} w={"100%"}>
+        <video autoPlay loop muted width={"100%"}>
+          <source
+            src="https://v4.cdnpk.net/videvo_files/video/free/video0485/large_watermarked/_import_61b4426beee026.13798649_FPpreview.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Box>
       <Heading
         bgGradient={"linear(to-tr,primary,secondary,highlight,teritory)"}
         bgClip={"text"}
@@ -42,7 +52,6 @@ const About = () => {
       </Heading>
       <Stack
         mx={{ base: "5%", md: "15%" }}
-        my={"5%"}
         display={"flex"}
         justifyContent={"space-between"}
         alignItems={"center"}
@@ -51,8 +60,9 @@ const About = () => {
         <Image
           src={Profile}
           alt="image"
-          boxSize={{ base: "35%", md: "25%" }}
-          borderRadius={"3xl"}
+          boxSize={{ base: "35%", md: "30%" }}
+          rounded={"full"}
+          opacity={0.8}
           animation={`3s ${pulseRing}  cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s `}
         />
         <Stack
@@ -96,6 +106,9 @@ const About = () => {
           flexDirection={{ base: "column", md: "row" }}
           gap={4}
           p={2}
+          // bg={
+          //   "url('https://v4.cdnpk.net/videvo_files/video/free/video0485/large_watermarked/_import_61b4426beee026.13798649_FPpreview.mp4')"
+          // }
         >
           <Stack
             w={{ base: "100%", md: "30%" }}
